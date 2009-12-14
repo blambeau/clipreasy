26879731c561ce2fddc2f8a134aad224a46df1a5
Bernard Lambeau <blambeau@gmail.com>
2009-12-14 15:05:26 +0000

    Provides basic configuration scheme.
    git-svn-id: http://svn.chefbe.net/clipreasy/trunk@9 5d269d64-8d56-4983-9dfe-c835c41a793b

--- lib/clipreasy.rb
+++ lib/clipreasy.rb
@@ -6,5 +6,21 @@ module CliPrEasy
   # Current version
   VERSION = "0.0.1".freeze
   
+  # Returns the configuration
+  def config
+    raise IllegalStateError, "CliPrEasy has not been started (no configuration loaded)"\
+      unless @config
+    @config
+  end
+  
+  # Starts the engine, loading the configuration from a whoami file.
+  # If no whoami file is provided the default one is loaded from project's root.
+  def self.start(whoami_file=nil)
+    whoami_file = File.join(File.dirname(__FILE__), '..', 'whoami')
+    @config = CliPrEasy::Config.load(whoami_file)
+  end
+  
 end
+require 'clipreasy/errors'
+require 'clipreasy/config'
 require 'clipreasy/engine'
