753dec64a0f687b147c8b82bca7fe5bf6e0b85dc
Bernard Lambeau <blambeau@gmail.com>
2010-01-04 13:06:28 +0000

    Moves current test to unit tests.
    git-svn-id: http://svn.chefbe.net/clipreasy/trunk@18 5d269d64-8d56-4983-9dfe-c835c41a793b

--- test/unit/test_all.rb
+++ test/unit/test_all.rb
@@ -1,4 +1,4 @@
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__),'..', 'lib'))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', '..', 'lib'))
 require 'clipreasy'
 require 'clipreasy/clipreasy_test'
 require 'test/unit'
