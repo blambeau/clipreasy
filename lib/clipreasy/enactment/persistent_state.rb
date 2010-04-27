c49ac9ed74fb801d1edde7e43b3e6fd673017af0
Bernard Lambeau <blambeau@gmail.com>
2010-04-27 17:14:12 +0200

    First working version of persistent executions.

---
+++ lib/clipreasy/enactment/persistent_state.rb
@@ -0,0 +1,91 @@
+module CliPrEasy
+  module Enactment
+    class PersistentState
+      
+      # Statement under execution
+      attr_reader :statement
+      
+      # Creates a new state instance
+      def initialize(parent, statement, relvar, tuple, evaluator)
+        @parent, @statement = parent, statement
+        @relvar, @tuple = relvar, tuple
+        @evaluator = evaluator
+      end
+      
+      # Starts a new process execution
+      def self.process_execution(relvar, process, evaluator = nil, &block)
+        tuple = relvar << {
+          :process_id   => process.identifier,
+          :statement_id => nil,
+          :parent_id    => nil,
+          :status       => :pending,
+          :started_at   => Time.now,
+          :ended_at     => nil,
+          :state        => {}
+        }
+        PersistentState.new(nil, process, relvar, tuple, evaluator || block)
+      end
+      
+      # Checks if this scope is currently opened
+      def pending?
+        tuple[:status] == :pending
+      end
+      
+      # Branches this scope as a new child one
+      def branch(statement)
+        tuple = @relvar << {
+          :process_id   => @tuple[:process_id],
+          :statement_id => statement.identifier,
+          :parent_id    => @tuple[:identifier],
+          :status       => :pending,
+          :started_at   => Time.now,
+          :ended_at     => nil,
+          :state        => {}
+        }
+        PersistentState.new(self, statement, @relvar, tuple, @evaluator)
+      end
+      
+      # Returns the parent state 
+      def parent
+        @parent ||= load_parent
+      end
+      
+      # Loads the parent state
+      def load_parent
+        return nil if @tuple[:parent_id].nil?
+        tuple = @relvar.restrict(:identifier => @tuple[:parent_id]).tuple_extract
+        PersistentState.new(@relvar, tuple, nil, @evaluator)
+      end
+      
+      # Returns the value of a variable
+      def get(variable)
+        @tuple[:state][variable] || (parent && parent.get(variable))
+      end
+      
+      # Sets the value of a given variable
+      def set(variable, value)
+        @tuple[:state][variable] = value
+        @relvar.restrict(:identifier => @tuple[:identifier]).update(:state => @tuple[:state])
+      end
+      
+      # Resume execution
+      def resume
+        statement.resume(self)
+      end
+    
+      # Close this scope and returns its parent  
+      def close
+        updated = {:status => :ended, :ended_at => Time.now}
+        @tuple = @tuple.merge(updated)
+        @relvar.restrict(:identifier => @tuple[:identifier]).update(updated)
+        parent
+      end
+      
+      # Evaluates an expression
+      def evaluate(expression)
+        @evaluator.call(expression, self)
+      end
+      
+    end # class PersistentState
+  end # module Enactment
+end # module CliPrEasy
---
+++ test/spec/enactment/persistent_state.spec
@@ -0,0 +1,40 @@
+require 'clipreasy'
+describe ::CliPrEasy::Enactment::PersistentState do
+  include ::CliPrEasy::Fixtures
+  
+  before(:all) do
+    @db = ::CliPrEasy::Persistence::Rubyrel::install_schema!(pgsql_test_database_handler)
+    @process = work_and_coffee_process
+    @process.save_on_relational_model(@db.model, @db.schema.namespace(:model))
+    @db.enactment.process_execution_state = []
+    @relvar = @db.enactment.process_execution_state
+  end
+  
+  # Returns an evaluator for this process
+  def evaluator
+    tired, too_much = nil, true
+    Kernel.lambda do |expression, state|
+      case expression.strip
+        when "tired?"
+          old, tired = tired, true
+          old
+        when 'too_much_coffee?'
+          old, too_much = too_much, true
+          old
+        when "true"
+          true
+        else
+          raise "Unexpected expression #{expression}"
+      end
+    end
+  end
+  
+  it "should support starting processes" do
+    state = ::CliPrEasy::Enactment::PersistentState.process_execution(@relvar, @process, evaluator)
+    terminals = @process.start(state)
+    until terminals.empty?
+      terminals = terminals.collect{|t| t.resume}.flatten
+    end
+  end
+  
+end
