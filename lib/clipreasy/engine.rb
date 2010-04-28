369ebdcdeb4d0e447e6ec66bf63c21e1be0c6f74
Bernard Lambeau <blambeau@gmail.com>
2010-04-28 09:23:08 +0200

    Adds a method to find statements in enacter.

--- lib/clipreasy/engine.rb
+++ lib/clipreasy/engine.rb
@@ -17,5 +17,11 @@ module CLiPrEasy
       @processes[process_id] ||= ::CliPrEasy::Persistence::Rubyrel::load_process(database, process_id)
     end
     
+    # Finds a statement inside a process
+    def statement(process_id, statement_id)
+      process = process(process_id)
+      process ? process.statement_by(:identifier, statement_id) : nil
+    end
+    
   end # class Engine
 end # module CliPrEasy
